dd51d1f68f4ef877dc136da964ac8805489265a4
sherkey <443229216@qq.com>
2015-08-06 17:49:58 +0800

    add python-mode org-mode

--- init.el
+++ init.el
@@ -10,6 +10,13 @@
   ("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
+;;org-mode
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/.emacs.d/org/work.org"))
+
 ;;ido mode
 (require 'ido)
 (ido-mode t)
@@ -20,6 +27,7 @@
 (color-theme-initialize)
 (color-theme-dark-blue2)
 
+;;auto-complete
 (add-to-list 'load-path "~/.emacs.d/elpa/auto-complete")   
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete/dict")
@@ -36,7 +44,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,4 +58,31 @@
 (global-linum-mode t)
 ;;开启括号匹配
 (show-paren-mode t)
+;;加一道竖线
+(add-to-list 'load-path "~/.emacs.d/plugin/")
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+ global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
+; python-mode
+(setq py-install-directory "~/.emacs.d/plugin/python-mode/")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
 
