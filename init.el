8faa2bdcbc31b615d6ac3de7a32474c7977df396
sherkey <443229216@qq.com>
2015-10-23 17:11:43 +0800

    indent sql

--- init.el
+++ init.el
@@ -124,3 +124,6 @@ charset
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+;;sql indent
+(eval-after-load "sql"
+'(load-library "sql-indent"))
